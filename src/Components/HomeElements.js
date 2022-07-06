d401a14ce36cf22530e788ce080cdbd616020889
palagashviliniika <n_palagashvili@cu.edu.ge>
2022-07-06 19:24:27 +0400

    Created Input component and worked with the basics

---
+++ src/Components/HomeElements.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Buttons =styled.div`
+
+`
+
+export const Inputs = styled.div`
+
+`
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "../Components/Button";
+import {Buttons, Inputs} from "../Components/HomeElements";
+import Input from "../Components/Input";
 
 const Home = ({variable, children}) => {
+    const [errors, setErrors] = useState();
     return (
         <div>
-            {variable} {children}
-            <Button>asd</Button>
-            <Button>Get Started</Button>
-            <Button number={variable}/>
+            {
+                errors &&
+                <div>errors</div>
+            }
+            <Buttons>
+                {variable} {children}
+                <Button>asd</Button>
+                <Button>Get Started</Button>
+                <button onClick={()=>{setErrors(true)}}>Start</button>
+                <Button number={variable}/>
+            </Buttons>
+
+            <Inputs>
+                <Input type={"name"} placeholder={"Name *"}/>
+                <Input type={"email"} placeholder={"Email Address *"}/>
+                <Input type={"email"} placeholder={"Email Address *"}/>
+            </Inputs>
         </div>
     );
 };
